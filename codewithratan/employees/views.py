059fc660e0138770da3c4ea1866b4ba2e8feb3a4
Rahul Kumar Yadav <ry573870@gmail.com>
2025-09-01 23:01:04 +0530

    first commit

---
+++ codewithratan/employees/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from employees.models import Employee
+# Create your views here.
+def employee_detail(request, pk):
+    employee = get_object_or_404(Employee,pk=pk)
+    return render(request, 'employee_detail.html')

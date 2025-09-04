0c2baa71a51e2a0c9841ba70ee8b10a53aa74f2c
Rahul Kumar Yadav <ry573870@gmail.com>
2025-09-04 16:50:08 +0530

    admin list display

--- ToDo/todo/admin.py
+++ ToDo/todo/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Task
 # Register your models here.
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task', 'isCompleted', 'updated_at')
+    search_fields = ('task',)
+
+admin.site.register(Task, TaskAdmin)

df2b7f8374067b42adaf6f91646161b221057ad6
Rahul Kumar Yadav <ry573870@gmail.com>
2025-09-04 13:14:18 +0530

    employee details page complete

--- codewithratan/employees/views.py
+++ codewithratan/employees/views.py
@@ -4,4 +4,7 @@ from employees.models import Employee
 # Create your views here.
 def employee_detail(request, pk):
     employee = get_object_or_404(Employee,pk=pk)
-    return render(request, 'employee_detail.html')
+    context = {
+        'employee': employee
+    }
+    return render(request, 'employee_detail.html', context)

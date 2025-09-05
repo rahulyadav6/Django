1faeb4342ac766c087859319f1a9146206ad555c
Rahul Kumar Yadav <ry573870@gmail.com>
2025-09-05 20:43:26 +0530

    completed task visible on UI

--- ToDo/todo_main/views.py
+++ ToDo/todo_main/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from todo.models import Task
 def home(request):
     tasks = Task.objects.filter(isCompleted=False).order_by('-updated_at')
+
+    completed_tasks = Task.objects.filter(isCompleted=True).order_by('-updated_at')
     context = {
         'tasks': tasks,
+        'completed_tasks' : completed_tasks
     }
     return render(request, 'home.html', context)

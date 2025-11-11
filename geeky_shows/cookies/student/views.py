41005894b6635477d505a2e231c53f8f9d09c1d2
Rahul Kumar Yadav <ry573870@gmail.com>
2025-11-11 09:16:08 +0530

    normal cookies understand

---
+++ geeky_shows/cookies/student/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def setcookie(request):
+    response =  render(request, 'student/setcookie.html')
+    response.set_cookie('token', 't123456', max_age=60)
+    return response
+
+def getcookie(request):
+    # token = request.COOKIES['token'] #it will give error if token is not set in cookie to prevent this use below sysntax
+    token = request.COOKIES.get('token', 'defaulttoken123')
+    return render(request, 'student/getcookie.html', {'token':token})
+
+def delcookie(request):
+    response =  render(request, 'student/delcookie.html')
+    response.delete_cookie('token')
+    return response
